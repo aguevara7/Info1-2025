f112c5ab7e833d6c396f39aefe5615432be9368c
Angela Guevara <angelaguevaraa07@gmail.com>
2025-06-22 19:16:24 -0300

    se agrego condicion de imc para el usuario

--- bmi.c
+++ bmi.c
@@ -10,9 +10,19 @@ int main(void) {
 	
 	imc = peso/(altura*altura);
 	printf ("Su indice es: %.2f\n", imc);
-	printf ("Indice|condicion\n");
+	printf ("\nIndice|condicion\n");
 	printf ("-----------------\n");
-	printf ("<18.5|Bajo peso\n 18.5 a 24.9|Normal\n 25.0 a 29.9|Sobrepeso\n >=30|Obesidad");
+	printf ("<18.5|Bajo peso\n 18.5 a 24.9|Normal\n 25.0 a 29.9|Sobrepeso\n >=30|Obesidad\n");
+	
+	if (imc < 18.5) 
+		printf("La condición del usuario es: Bajo peso\n");
+	else if (imc < 25.0)
+		printf("La condición del usuario es: Normal\n");
+	else if (imc < 30.0)
+		printf("La condición del usuario es: Sobrepeso\n");
+	else
+		printf("La condición del usuario es: Obesidad\n");
+
 	return 0;
 }
 
